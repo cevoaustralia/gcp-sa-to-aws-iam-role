bbfd76b78282df5ca67675acc87032ca7417317a
Colin Panisset <colin.panisset@cevo.com.au>
2019-06-28 16:28:24 +1000

    Add stub function to get OIDC token

---
+++ gcp/generate_oidc_token.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#
+# This code must run on a VM instance with a service account associated with it.
+# The service account must be granted the 'Service Account Token Creator' IAM Role
+
+import googleapiclient.discovery
+import json
+import os
+import requests
+
+SERVICE_ACCOUNT = os.getenv("SERVICE_ACCOUNT", "default")
+BASE_URL = 'http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/'
+AUDIENCE = 'potato'
+FORMAT = 'full'
+METADATA_HEADERS = { 'Metadata-Flavor': 'Google' }
+
+# Construct a URL with the audience and format
+url = BASE_URL + f"{SERVICE_ACCOUNT}/identity?audience={AUDIENCE}&format={FORMAT}"
+
+r = requests.get(url, headers=METADATA_HEADERS)
+token = r.text
+
+print(token)
